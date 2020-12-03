ea37a20d2ee8dd55c434e025f72a29e81a8a16af
Eric Carlson <ercarlson07@gmail.com>
2020-12-03 13:51:40 -0500

    improved. don't need try/except if check for exceeded columns before tree check. thanks to aj's code lol

--- aoc2020/day3/src/main.py
+++ aoc2020/day3/src/main.py
@@ -1,38 +1,29 @@
 #!/usr/bin/env python
 from adventofcode.aoc2020.helper_funcs import get_input, multiply_list
 
-def main(inp, slope):
+def main(inp, right_steps, down_steps):
     trees = 0
     col = 0
     row = 0
     line_length = len(inp[0])
 
     for _ in range(len(inp) - 1):
-        # Move the slope. Slope is right 3, down 1
-        col += slope[0]
-        row += slope[1]
+        col += right_steps
+        row += down_steps
 
-        # Catch if rows are exceeded when moving down is > 1
+        # Sim line extension
+        if col >= line_length:
+            col -= line_length
+
+        # Return if rows are exceeded when down_steps > 1
         if row > len(inp):
             return trees
 
-        # Check character for tree or snow
-        try:
-            tree = check_for_tree(inp, row, col)
-        except IndexError:
-            # move index back to start of line 
-            col -= line_length
-            tree = check_for_tree(inp, row, col)
-
-        if tree:
+        # Check spot for tree or snow
+        if inp[row][col] == '#':
             trees += 1
 
     return trees
-
-def check_for_tree(inp, row, col):
-    if inp[row][col] == '#':
-        return True
-    return False
 
 
 if __name__ == '__main__':
@@ -41,7 +32,7 @@ if __name__ == '__main__':
     slopes = ((1,1), (3,1), (5,1), (7,1), (1,2))
     results = [] 
     for run, slope in enumerate(slopes):
-        trees_hit = main(inp, slope)
+        trees_hit = main(inp, *slope)
         results.append(trees_hit)
         run += 1
         print(f'Run {run} with a slope of {slope} hit {trees_hit} trees!')
